16ae788829dcd76a1288e419ef050a018375678c
Christian Diener <ch.diener@gmail.com>
2025-11-04 15:15:19 +0100

    remove redundant entries

--- bin/match.R
+++ bin/match.R
@@ -121,8 +121,8 @@ find_taxon <- function(taxid, gb_taxa, gb_summary, col, db) {
         refseq_category = refseq_category, assembly_level = assembly_level,
         seqlength=seqlength, name = name, genome_type = genome_type
     )
-    results <- results[order(id, -seqlength)] %>%
-        unique(by=c("id", "db", "matched_taxid", "assembly_level", "name", "genome_type"))
+    results <- results[order(-seqlength)] %>%
+        unique(by=c("db", "matched_taxid", "assembly_level", "name", "genome_type"))
 
     flog.info("Found %d unique records for %s with a total size of %g MBps.",
         nrow(result), taxid, sum(seqlength) / 1e6, ncol(result))
