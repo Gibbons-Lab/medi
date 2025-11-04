1b850400b0947aa3614e9175811ab24d212b2bd6
Christian Diener <ch.diener@gmail.com>
2025-11-04 13:11:42 +0100

    provide more info

--- bin/match.R
+++ bin/match.R
@@ -121,6 +121,8 @@ find_taxon <- function(taxid, gb_taxa, gb_summary, col, db) {
         refseq_category = refseq_category, assembly_level = assembly_level,
         seqlength=seqlength, name = name, genome_type = genome_type
     )
+    flog.info("Found %d records for %s with a total size of %g MBps.",
+        nrow(result), taxid, result[, sum(seqlength) / 1e6])
 
     return(result)
 }
